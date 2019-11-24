d98387729bbe53e6bc6c7984d9b15b9aafe80082
Nguyen Huy <scythedead.h@gmail.com>
2019-11-24 14:24:35 +0700

    [master] check husky hooks

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import styles from './styles.js';
 import { withStyles } from '@material-ui/core';
 import { ThemeProvider } from '@material-ui/core/styles';
+import styles from './styles.js';
 import Taskboard from '../Taskboard/index.js';
 import theme from '../../commons/theme';
 
